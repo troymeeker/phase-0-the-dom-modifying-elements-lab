6e63e746cae32e63b9528d56ffb218f4ba7c84cb
troymeeker <troymeeker1@gmail.com>
2021-07-23 15:39:55 -0700

    complete

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const main = document.querySelector('#main')
-
-main.remove();
-
-const newHeader = document.createElement('h1');
-
-newHeader.setAttribute("id","victory"); 
-
-
-newHeader.textContent = "TROY is the champion"
-
-document.body.append(newHeader); 
+const newHeader = document.getElementById('victory');
